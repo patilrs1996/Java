f425bdd2d05669e20247f8de48b164d81968ed91
patilrs1996 <34850757+patilrs1996@users.noreply.github.com>
2017-12-26 11:39:32 +0530

    Add files via upload

---
+++ StarPattern.java
@@ -0,0 +1,71 @@
+/*import java.io.*;
+
+import java.util.*;
+
+public class StarPattern
+{
+ 
+  static public void main(String[] args)
+
+  {
+
+     Scanner sc=new Scanner(System.in);
+
+     System.out.println("Enter number :");
+
+     	for(int r=1;r<=4;r++)
+	{	
+			
+		for(int c=1;c<=r;c++)
+		{
+			
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+   
+ }
+
+}
+O/P:
+*
+**
+***
+****
+
+*/
+
+
+import java.io.*;
+
+import java.util.*;
+
+public class StarPattern
+{
+ 
+  static public void main(String[] args)
+
+  {
+
+     Scanner sc=new Scanner(System.in);
+
+       	for(int r=0;r<4;r++)
+	{	
+			
+		for(int c=4;c>r;c--)
+		{
+			
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+   
+ }
+
+}
+
+O/P:
+****
+***
+**
+*
